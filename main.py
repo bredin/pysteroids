53891ef652434ec7f6d83665dafc02be85b53006
bredin <insanityindustries@gmail.com>
2025-07-28 11:50:03 +1200

    game screen closing

--- main.py
+++ main.py
@@ -3,9 +3,22 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+            
+        screen.fill((0, 0, 0))  # Clear the screen with black
+        pygame.display.flip()  # Update the display
+
+
+#    print("Starting Asteroids!")
+#    print(f"Screen width: {SCREEN_WIDTH}")
+#    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
