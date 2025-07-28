dc9d961d5558813e6763cc89a12af550041795e3
bredin <insanityindustries@gmail.com>
2025-07-28 12:50:48 +1200

    framerate timer added

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -15,10 +18,7 @@ def main():
         screen.fill((0, 0, 0))  # Clear the screen with black
         pygame.display.flip()  # Update the display
 
-
-#    print("Starting Asteroids!")
-#    print(f"Screen width: {SCREEN_WIDTH}")
-#    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60)/1000  # Limit to 60 FPS
 
 
 if __name__ == "__main__":
